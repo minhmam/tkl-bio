30ab40e3c6831f89e0adbc48cf4139da190cbfd2
Phạm Trường Minh <88191963+minhmam@users.noreply.github.com>
2026-03-06 19:24:19 +0700

    change name

--- src/presentation/pages/BioPage.tsx
+++ src/presentation/pages/BioPage.tsx
@@ -47,7 +47,7 @@ export function BioPage() {
       <section className="page-wrap">
         <Header
           title=""
-          subtitle="Trang trưng bày của Phùng Hoài Thương"
+          subtitle="Trang trưng bày của Trần Khánh Linh"
           actionLabel="Vào trang admin"
           actionPath="/admin"
         />
